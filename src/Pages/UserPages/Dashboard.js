b7a864e3d0bc6ec7e8f0ef86ae3f162f44abae01
nagarajan091792 <87260947+nagarajan091792@users.noreply.github.com>
2023-01-21 11:19:27 +0530

    Update Dashboard.js

--- src/Pages/UserPages/Dashboard.js
+++ src/Pages/UserPages/Dashboard.js
@@ -29,7 +29,7 @@ function Dashboard(){
         </div>
      {
                 isloading ? <div class="d-flex justify-content-center">
-  <div class="spinner-border" role="status">
+  <div class="spinner-border m-5" role="status">
     <span class="sr-only">Loading...</span>
   </div>
 </div> :  
